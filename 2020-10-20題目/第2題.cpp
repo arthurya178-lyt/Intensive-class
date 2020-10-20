b5dfbee139f037a2efa8e12efe3333056cf20498
arthurya178-pccu <59197410+arthurya178-pccu@users.noreply.github.com>
2020-10-20 23:35:44 +0800

    2020/10/20-11:35

---
+++ 2020-10-20題目/第2題.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	srand(unsigned(time(NULL)));				//設定時間種子
+	int bottom = 0, top = 9999,guessTime = 0;	//設頂跟底並設定猜測計數器為0
+	int randomNumber = rand()%10000;			//設定隨機數字
+	int input;									//設定使用者輸入數值
+	while (true)
+	{
+		guessTime++;							//猜測數字加1
+		cout << bottom << "~" << top << endl;	//輸出範圍
+		cin >> input;							//使用者輸入
+		if (input == randomNumber)				//猜到數字
+		{
+			cout << "Guessed!!!" << endl;			//顯示
+			cout << "Time:" << guessTime << endl;	//顯示次數
+			break;									//中斷迴圈
+		}
+		else if (input < randomNumber && input > bottom)	//數字小於所隨機數字並且輸入值大於底
+			bottom = input;
+		else if (input > randomNumber && input < top)		//數字大於所隨機數字並且輸入值小於頂
+			top = input;
+	}
+}
---
+++ 2020-10-20題目/第3題.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char input;				//輸入的字元
+	cin >> input;			//使用者輸入
+	int inputAscii = (int)input, outputAscii;	//轉換成ASCII
+	if (inputAscii >= 65 && inputAscii <= 90)	//判斷是否為大寫
+		outputAscii = inputAscii + 32;
+	else if (inputAscii >= 97 && inputAscii <= 122)//判斷是否為大寫
+		outputAscii = inputAscii - 32;
+	else											//其他
+		outputAscii = -1;
+	if (outputAscii != -1)					//確認是否為警示值
+		cout << (char)outputAscii;			//輸出
+	else
+		cout << "Error!!!";					//顯示為錯誤
+}
+//Ascii的對照表:http://kevin.hwai.edu.tw/~kevin/material/CLanguage/ASCII.htm
