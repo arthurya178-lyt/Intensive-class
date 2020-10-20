702a27dd5bb21e1166d7a9a96a12f65e5bbd6c19
arthurya178-pccu <59197410+arthurya178-pccu@users.noreply.github.com>
2020-10-20 23:39:12 +0800

    Update 第1題.cpp

--- 2020-10-20題目/第1題.cpp
+++ 2020-10-20題目/第1題.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-	int numA = 0, numB = 1, numC = 1,input;  //設定 N-2 跟 N-1 跟 N 的數字
+	int numA = 0, numB = 1, numC = 1,input; 		 	//設定 N-2 跟 N-1 跟 N 的數字
 	cin >> input;							//輸入範圍
-	while (numC < input)					//檢查N是否比範圍大
+	while (numC < input)						//檢查N是否比範圍大
 	{
-		cout << numC << " ";				//輸出N
+		cout << numC << " ";					//輸出N
 		numC = numA + numB;					//使N等於N-1 加 N-2
 		numA = numB;						//使N-2 指定為N-1的數字
 		numB = numC;						//使N-1 指定為N的數字
